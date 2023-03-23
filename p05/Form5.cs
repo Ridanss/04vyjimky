50e76142b00c52e87e2c250ad77ebe42ff6cb2f7
Ridanss <pavlonk.04@spst.eu>
2023-03-23 08:50:26 +0100

    p05

--- p05/Form5.cs
+++ p05/Form5.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,6 +22,18 @@ namespace p05
         {
             int[] ints = new int[2];
             string[] strings = new string[2];
+            if(PlatnostRc(ints, strings))
+            {
+                MessageBox.Show("Rodné číslo je platné");
+            }
+            else
+            {
+                MessageBox.Show("Rodné číslo je neplatné");
+            }
+        }
+
+        public bool PlatnostRc(int[] ints, string[] strings)
+        {
             try
             {
                 strings = textBox1.Text.Split('/');
@@ -28,13 +41,25 @@ namespace p05
                 {
                     ints[0] = Int32.Parse(strings[0]);
                     ints[1] = Int32.Parse(strings[1]);
+
+                    if ((ints[0] + ints[1]) % 11 == 0)
+                    {
+                        int rokc = Int32.Parse(strings[0].Substring(0, 2));
+                        int mesc = Int32.Parse(strings[0].Substring(2, 2));
+                        int denc = Int32.Parse(strings[0].Substring(4, 2));
+                        if (rokc <= DateTime.Today.Year) rokc += 2000;
+                        else rokc += 1900;
+                        if (mesc > 50) mesc -= 50;
+                        DateTime naroz = new DateTime(rokc, mesc, denc);
+                        if (naroz != null) return true;
+                    }
                 }
-                else MessageBox.Show("r.číslo je neplatné");
-                if (ints[0] % 11 != 0) MessageBox.Show("r.číslo je neplatné");
+                return false;
             }
             catch (FormatException ex)
             {
                 MessageBox.Show(ex.Message);
+                return false;
             }
         }
     }
